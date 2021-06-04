11889564b043c831b4594fa8e5cc71b2d3f07eae
halil-kaya <hibrahimkayahik@gmail.com>
2021-06-04 18:37:23 +0300

    sign-in ve sign-up işlemleri eklendi

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -3,7 +3,6 @@ import SHOP_DATA from './shop.data.js'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
 
-
 class ShopPage extends React.Component {
 
     constructor(props){
@@ -13,7 +12,6 @@ class ShopPage extends React.Component {
             collections : SHOP_DATA
         }
     }
-
 
     render(){
 
